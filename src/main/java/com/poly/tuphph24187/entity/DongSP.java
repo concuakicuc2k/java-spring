66b14190966891c47a7cc9c921d2c4ae286c147a
tuphph24187 <concuakicuc2k@gmail.com>
2023-06-01 22:55:15 +0700

    updateee

---
+++ src/main/java/com/poly/tuphph24187/entity/DongSP.java
@@ -0,0 +1,28 @@
+package com.poly.tuphph24187.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "dong_sp")
+public class DongSP {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "ma")
+    private String ma;
+
+    @Column(name = "ten")
+    private String tenDongSP;
+
+}
